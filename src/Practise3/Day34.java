da7bdab65adb086c02355f972a0b0376df056224
jeevan <beherajeevanjyoti117@gmail.com>
2025-02-02 14:50:54 +0530

    develeop branch

---
+++ src/Practise3/Day34.java
@@ -0,0 +1,15 @@
+package Practise3;
+
+public class Day34 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 10;
+		while(i>0) {
+			System.out.println(i);
+			i--;
+		}
+
+	}
+
+}
